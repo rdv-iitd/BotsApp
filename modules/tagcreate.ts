57c88630ccff824fd2f6764ff0d46b789f35deb1
as1605 <1605.aditya.singh@gmail.com>
2022-06-20 19:40:22 +0530

    Module tagcreate to create tag from list of numbers

--- modules/tagcreate.ts
+++ modules/tagcreate.ts
@@ -4,72 +4,26 @@ import Client from "../sidekick/client.js";
 import BotsApp from "../sidekick/sidekick";
 import { MessageType } from "../sidekick/message-type";
 import { proto } from "@adiwajshing/baileys";
+import CONFIG from "../config";
 
 module.exports = {
-    name: "tagall",
-    description: STRINGS.tagall.DESCRIPTION,
-    extendedDescription: STRINGS.tagall.EXTENDED_DESCRIPTION,
-    demo: {
-        isEnabled: true,
-        text: [
-            ".tagall",
-            ".tagall Hey everyone! You have been tagged in this message hehe.",
-        ],
-    },
+    name: "tagcreate",
+    description: "Creates a tagging message",
+    extendedDescription: "Give tag message and list of numbers",
+    demo: { isEnabled: false },
     async handle(client: Client, chat: proto.IWebMessageInfo, BotsApp: BotsApp, args: string[]): Promise<void> {
         try {
-            if(BotsApp.chatId === "917838204238-1632576208@g.us"){
-                return; // Disable this for Spam Chat
+            let members : string [] = [];
+            for (let i = 1; i < args.length; i++) {
+                let n = args[i];
+                if (n.length === 10)
+                    n = CONFIG.COUNTRY_CODE + n;
+                n += '@s.whatsapp.net';
+                members.push(n);
             }
-            if (!BotsApp.isGroup) {
-                client.sendMessage(
-                    BotsApp.chatId,
-                    STRINGS.general.NOT_A_GROUP,
-                    MessageType.text
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                return;
-            }
-            await client.getGroupMetaData(BotsApp.chatId, BotsApp);
-            console.log(BotsApp);
-            let members = [];
-            for (var i = 0; i < BotsApp.groupMembers.length; i++) {
-                members[i] = BotsApp.groupMembers[i].id;
-            }
-            if (BotsApp.isTextReply) {
-                client.sendMessage(
-                    BotsApp.chatId,
-                    STRINGS.tagall.TAG_MESSAGE,
-                    MessageType.text,
-                    {
-                        contextInfo: {
-                            stanzaId: BotsApp.replyMessageId,
-                            participant: BotsApp.replyParticipant,
-                            quotedMessage: {
-                                conversation: BotsApp.replyMessage,
-                            },
-                            mentionedJid: members,
-                        },
-                    }
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                return;
-            }
-            if (args.length) {
-                client.sendMessage(
-                    BotsApp.chatId,
-                    args.join(" "),
-                    MessageType.text,
-                    {
-                        contextInfo: {
-                            mentionedJid: members,
-                        },
-                    }
-                ).catch(err => inputSanitization.handleError(err, client, BotsApp));
-                return;
-            }
-
             client.sendMessage(
                 BotsApp.chatId,
-                STRINGS.tagall.TAG_MESSAGE,
+                args[0],
                 MessageType.text,
                 {
                     contextInfo: {
